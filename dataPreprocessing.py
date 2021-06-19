649c6844bd6f2a5fe39446a1c308550b3a9d839e
Gfecito <71032552+Gfecito@users.noreply.github.com>
2021-06-19 11:22:39 -0500

    Initial workspace setup.
    Please add your names on both .py files

---
+++ dataPreprocessing.py
@@ -0,0 +1,55 @@
+# Authors:
+#             GOMEZ MAQUEO ANAYA, Santiago
+#             HEBRARD, Emilie
+#             SHEN, Yue
+
+
+# Date of start:            19/06/2021
+# Date of finish:           -/-/-
+
+
+
+# This file will use the Pandas library to preprocess our databases
+# Ideally after executing this file with any relevant databases we'll get dataframes
+# That we can efficiently use to train our model.
+ 
+ 
+
+ 
+
+#  -- IMPORTS AND LIBRARIES --
+
+
+
+
+
+
+# -- INPUT HANDLING --
+# Here we take a database and store it in a format that's easy to preprocess.
+
+
+
+
+
+
+# -- DATABASE PREPROCESSING --
+# We preprocess our data.
+
+
+
+
+
+
+
+# -- OUTPUT GENERATION --
+# We build our dataframe from our preprocessing.
+
+
+
+
+
+
+
+
+# -- OUTPUT HANDLING --
+# We take our dataframe and write a new file with it.
